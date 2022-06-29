6487fdf7c27c5f56aebe1f613cf84e1379030ce0
Alcazaba <99512955+Alcazaba@users.noreply.github.com>
2022-06-30 00:46:03 +0200

    Tableau de bord : charts

--- src/Controller/TableauDeBordController.php
+++ src/Controller/TableauDeBordController.php
@@ -1,9 +1,8 @@
 <?php
 
-
 namespace App\Controller;
 
-use App\Entity\Transactions;
+
 use App\Repository\CategoriesRepository;
 use App\Repository\TransactionsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,6 +10,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
 use Symfony\UX\Chartjs\Model\Chart;
+use App\Entity\Transactions;
+use App\Form\Transactions1Type;
+
+
+
 
 class TableauDeBordController extends AbstractController
 {
@@ -18,40 +22,116 @@ class TableauDeBordController extends AbstractController
 
     public function index(TransactionsRepository $transactionsRepository, CategoriesRepository $categoriesRepository, ChartBuilderInterface $chartBuilder): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $userId = $this->getUser()->getId();
         $labels = [];
-        $datasets = [];
+        $datasetsDepenses = [];
+        $datasetsRecettes = [];
         $categories = $categoriesRepository->findAll();
-        $transactions = $transactionsRepository->findAll();
+        $nbCategoriesDepense = 0;
+        $nbCategoriesRecettes = 0;
+        $transactions =
+            $transactionsRepository->findBy(array("user" => $userId));
+        $categoriesDepenses = [];
+        $categoriesRecettes = [];
         foreach ($categories as $data) {
-            if ($data->getComptabilite() == "1") {
+            if ($data->getComptabilite() == "dépense") {
                 $categoriesDepenses[] = $data->getNomCategorie();
+                $nbCategoriesDepense++;
             } else {
                 $categoriesRecettes[] = $data->getNomCategorie();
+                $nbCategoriesRecettes++;
             }
         }
 
+        $depensesParCategories = array_flip($categoriesDepenses);
+        foreach ($depensesParCategories as $key => $data) {
+            $depensesParCategories[$key] = 0;
+        }
+        $recettesParCategories = array_flip($categoriesRecettes);
+        foreach ($recettesParCategories as $key => $data) {
+            $recettesParCategories[$key] = 0;
+        }
+
         foreach ($transactions as $data) {
-            $labels[] = $data->getDateTransaction()->format('d-m-Y');
             if (in_array($data->getCategorie(), $categoriesDepenses)) {
-                $datasets[] = $data->getMontantTransaction();
+                $datasetsDepenses[] = $data->getMontantTransaction();
+                foreach ($depensesParCategories as $key => $value) {
+                    if ($key == $data->getCategorie()) {
+                        $depensesParCategories[$key]
+                            =  $depensesParCategories[$key] + $data->getMontantTransaction();
+                    }
+                }
+            } else {
+                $datasetsRecettes[] = $data->getMontantTransaction();
+                foreach ($recettesParCategories as $key => $value) {
+                    if ($key == $data->getCategorie()) {
+                        $recettesParCategories[$key]
+                            =  $recettesParCategories[$key] + $data->getMontantTransaction();
+                    }
+                }
             }
         }
-        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
-        $chart->setData([
-            'labels' => $labels,
+
+
+
+
+
+
+
+        /* Total dépenses / recettes */
+        $chart1 = $chartBuilder->createChart(Chart::TYPE_DOUGHNUT);
+        $chart1->setData([
+            'labels' => ["Dépenses", "Recettes"],
             'datasets' => [
                 [
-                    'label' => 'My First dataset',
-                    'backgroundColor' => 'rgb(255, 99, 132)',
-                    'borderColor' => 'rgb(255, 99, 132)',
-                    'data' => $datasets,
+                    'backgroundColor' => ['rgb(255, 99, 132)', '#009b81'],
+                    'data' => [array_sum($datasetsDepenses), array_sum($datasetsRecettes)],
 
                 ]
             ],
         ]);
+
+        /* Détail dépenses */
+        $chart2 = $chartBuilder->createChart(Chart::TYPE_BAR);
+        $chart2->setData([
+            'title' => "Hello",
+            'labels' => $categoriesDepenses,
+            'datasets' => [
+                [
+                    'label' => 'Dépenses',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' =>  $depensesParCategories,
+
+                ]
+            ],
+        ]);
+
+        /* Détail recettes */
+        $chart3 = $chartBuilder->createChart(Chart::TYPE_BAR);
+        $chart3->setData([
+            'title' => "Hello",
+            'labels' => $categoriesRecettes,
+            'datasets' => [
+                [
+                    'label' => 'Recettes',
+                    'backgroundColor' => '#009b81',
+                    'data' =>  $recettesParCategories,
+
+                ]
+            ],
+        ]);
+
+
+
+
         return $this->render('tableau_de_bord/index.html.twig', [
             'croissants' => $transactionsRepository->findAll(),
-            'chart' => $chart,
+            'chart1' => $chart1,
+            'chart2' => $chart2,
+            'chart3' => $chart3,
         ]);
     }
 }
