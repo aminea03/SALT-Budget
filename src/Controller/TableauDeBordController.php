7cb6eade3fda8c77a6cb154ae6c7484c0ac73f0b
Alcazaba <99512955+Alcazaba@users.noreply.github.com>
2022-07-03 15:43:03 +0200

    Tableau de bord Fix

--- src/Controller/TableauDeBordController.php
+++ src/Controller/TableauDeBordController.php
@@ -38,7 +38,7 @@ class TableauDeBordController extends AbstractController
         $currentYearMonthsDepenses = ["01" => 0, "02" => 0, "03" => 0, "04" => 0, "05" => 0, "06" => 0, "07" => 0, "08" => 0, "09" => 0, "10" => 0, "11" => 0, "12" => 0];
         $currentYearMonthsRecettes = ["01" => 0, "02" => 0, "03" => 0, "04" => 0, "05" => 0, "06" => 0, "07" => 0, "08" => 0, "09" => 0, "10" => 0, "11" => 0, "12" => 0];
         foreach ($categories as $data) {
-            if ($data->getComptabilite() == "dépense") {
+            if ($data->getComptabilite() == "Dépense") {
                 $categoriesDepenses[] = $data->getNomCategorie();
                 $nbCategoriesDepense++;
             } else {
